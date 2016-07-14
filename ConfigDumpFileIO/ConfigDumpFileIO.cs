0b0b2b04fa3b9504207dc43b309bd7107338a465
andrewhaydenr <andrewhaydenr@gmail.com>
2016-07-14 12:23:49 -0400

    Initial Commit

--- ConfigDumpFileIO/ConfigDumpFileIO.cs
+++ ConfigDumpFileIO/ConfigDumpFileIO.cs
@@ -3,37 +3,35 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
 
 namespace ConfigDumpFileIO
 {
     public class DLLEntry
     {
-        static StringBuilder sB = new StringBuilder("");
-        static ManualResetEvent mre = new ManualResetEvent(false);
-        [DllExport("_RVExtension@12", CallingConvention = System.Runtime.InteropServices.CallingConvention.Winapi)]
-        public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string function)
+        static StreamWriter outputFile;
+        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+        public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string input)
         {
-            var colonIndex = function.IndexOf(":");
-            var operationType = function.Substring(0, colonIndex);
-            var stringToWrite = function.Substring(colonIndex + 1, function.Length - colonIndex - 1);
+            var colonIndex = input.IndexOf(":");
+            var operationType = input.Substring(0, colonIndex);
+            var stringToWrite = input.Substring(colonIndex + 1, input.Length - colonIndex - 1);
             if (operationType.Equals("open"))
             {
+                string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string filePath = Path.Combine(assemblyFolder, stringToWrite);
+                outputFile = new StreamWriter(filePath);
                 output.Append("true");
                 return;
             }
             else if(operationType.Equals("write"))
             {
-                sB.AppendLine(stringToWrite);
+                outputFile.WriteLine(stringToWrite);
                 output.Append("true");
                 return;
             }
             else if(operationType.Equals("close"))
             {
-                string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                string filePath = Path.Combine(assemblyFolder, "ConfigDump.cpp");
-                File.WriteAllText(filePath, sB.ToString());
-                sB = new StringBuilder("");
+                outputFile.Close();
                 output.Append("true");
                 return;
             }

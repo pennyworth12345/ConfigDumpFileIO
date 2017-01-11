1bd4a9574e17283c3a6112e0d28a028c48076b68
Pennyworth <pennyworth12345@gmail.com>
2017-01-11 01:21:40 -0500

    Added compatibility with 64 bit client and updated build configuration

---
+++ ConfigDumpFileIO_x64/ConfigDumpFileIO_x64.cs
@@ -0,0 +1,42 @@
+﻿using RGiesecke.DllExport;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConfigDumpFileIO_x64
+{
+    public class DLLEntry
+    {
+        static StreamWriter outputFile;
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+        public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string input)
+        {
+            var colonIndex = input.IndexOf(":");
+            var operationType = input.Substring(0, colonIndex);
+            var stringToWrite = input.Substring(colonIndex + 1, input.Length - colonIndex - 1);
+            if (operationType.Equals("open"))
+            {
+                string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string filePath = Path.Combine(assemblyFolder, stringToWrite);
+                outputFile = new StreamWriter(filePath);
+                output.Append("true");
+                return;
+            }
+            else if (operationType.Equals("write"))
+            {
+                outputFile.WriteLine(stringToWrite);
+                output.Append("true");
+                return;
+            }
+            else if (operationType.Equals("close"))
+            {
+                outputFile.Close();
+                output.Append("true");
+                return;
+            }
+
+            output.Append("false");
+        }
+    }
+}
